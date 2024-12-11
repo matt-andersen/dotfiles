1b30e3d83b88833a76e37850662baeb8801342f4
matt-andersen <mattandersen0@gmail.com>
2024-12-11 16:21:35 +1000

    Create bashrc.sh

---
+++ bashrc.sh
@@ -0,0 +1,10 @@
+cat <<EOF > ~/.bashrc
+alias gst='git status'
+alias gaa='git add -A'
+alias gcm='git commit -m'
+alias gco='git checkout'
+alias gpo='git push origin'
+alias gpl='git pull'
+alias gfk='git reset HEAD --hard && git clean -fd'
+EOF
+source ~/.bashrc
